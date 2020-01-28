3cde42d3fcac18c00c05b416483e5f0483f30722
kumarhemant10 <hkchauhan022@gmail.com>
2020-01-28 07:29:43 +0530

    default user added

--- vite_api/src/main/java/com/hk/prj/vite/Application.java
+++ vite_api/src/main/java/com/hk/prj/vite/Application.java
@@ -1,19 +1,42 @@
 package com.hk.prj.vite;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.hk.prj.vite.model.User;
+import com.hk.prj.vite.repository.UserRepository;
 
 /**
  * entry point of application
  */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-
+	
+	@Autowired UserRepository userRepository;
+	@Autowired PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	//initial users 
+	
+	@Bean
+	InitializingBean sendDatabase() {
+	    return () -> {
+	    	User user = new User();
+	    	user.setActive(Boolean.TRUE);
+	    	user.setUserName("hkchauhan022@gmail.com");
+	    	user.setPassword(passwordEncoder.encode("hemant123"));
+	    	user.setEmail("hkchauhan022@gmail.com");
+	        userRepository.save(user);
+	      };
+	   }
 	
 
 }
