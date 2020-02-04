d9954ada146bc117768f8949ad1ed01d87435151
kumarhemant10 <hkchauhan022@gmail.com>
2020-02-04 21:33:28 +0530

    exposing header authorization

--- vite_api/src/main/java/com/hk/prj/vite/config/WebSecurityConfig.java
+++ vite_api/src/main/java/com/hk/prj/vite/config/WebSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.hk.prj.vite.security.JwtConfig;
 import com.hk.prj.vite.security.JwtTokenAuthenticationFilter;
@@ -36,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.cors().and()
 		.csrf().disable()
 		// make sure we use stateless session; session won't be used to store user's state.
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -62,4 +66,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);        
+    }
 }
